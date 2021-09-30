842bc9ff56fa299785552f2d387bd73b2be3f11d
zivojinminic <zivojinminic05@gmail.com>
2021-09-30 16:44:04 +0200

    Map

---
+++ ex04/src/index.js
@@ -0,0 +1,10 @@
+// Only change code below this line
+let arena = new Map();
+
+arena.set("author", "Gigi");
+arena.set("publisher", "Mistral");
+arena.set("numberOfPages", 28);
+arena.set("year", 2021);
+// Only change code above this line
+for(let value of arena) console.log(value);
+module.exports = arena;
